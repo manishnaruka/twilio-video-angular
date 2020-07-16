192f2444198c7c78b6da741c4648eb1b8b7bdda5
Manish Naruka <manishn.siyatechventures@gmai.com>
2020-07-16 19:46:46 +0530

    connect to twilio room

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { connect } from 'twilio-video';
 })
 export class AppComponent implements OnInit {
   ngOnInit(){
-    connect('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzI0Yjk1NDQwN2E0NTg3ZDBjNzE0YjMxNGMxZTI0YjkxLTE1OTQ5MDI1NDUiLCJpc3MiOiJTSzI0Yjk1NDQwN2E0NTg3ZDBjNzE0YjMxNGMxZTI0YjkxIiwic3ViIjoiQUNhYjhiMTA4NmUyOWZlMzc5MTE4YzcwYjM2OTRkMGRkYyIsImV4cCI6MTU5NDkwNjE0NSwiZ3JhbnRzIjp7ImlkZW50aXR5IjoibWFuaXNoIiwidmlkZW8iOnsicm9vbSI6Ik9ua28gcGVyc29uYWwgcm9vbSJ9fX0.IwmShXGDySO7-RVQ2uKXUVXYaMmygebR-J2lcMK6Go0', { name:'onko personal room' }).then(room => {
+    connect('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzM1MGFkMDIxMGUyYThiMDY1OWExNGM5YjMwZWUwYWM1LTE1OTQ5MDYyMzYiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJhZG1pbkBnbWFpbC5jb20iLCJ2aWRlbyI6eyJyb29tIjoiT25rbyBwZXJzb25hbCByb29tIn19LCJpYXQiOjE1OTQ5MDYyMzYsImV4cCI6MTU5NDkwOTgzNiwiaXNzIjoiU0szNTBhZDAyMTBlMmE4YjA2NTlhMTRjOWIzMGVlMGFjNSIsInN1YiI6IkFDYWI4YjEwODZlMjlmZTM3OTExOGM3MGIzNjk0ZDBkZGMifQ.YiNMRiGQzB3W-oMrlMaQ0gvMh1WEzUuHwMAms82ySKY', { name:'onko personal room' }).then(room => {
       console.log(`Successfully joined a Room: ${room}`);
       room.on('participantConnected', participant => {
         console.log(`A remote Participant connected: ${participant}`);
